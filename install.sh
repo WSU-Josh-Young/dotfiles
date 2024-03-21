5e645916c19425222443a525e9f4685ed6332a06
WSU-Josh-Young <young.599@wright.edu>
2024-03-21 16:06:30 -0400

    script

---
+++ install.sh
@@ -0,0 +1,16 @@
+# Print the user who called the script
+username=$(whoami)
+echo "The user who called this script is: $username"
+# Check if the script is run as root
+if [[ "$(id -u)" -eq 0 ]]; then
+	echo "This script is run as root."
+	if [[ "$(which apt)" ]]; then
+		echo "apt is installed."
+	else
+		echo "apt is not installed." 1>&2
+		exit 1
+	fi
+else
+	echo "This script is not run as root." 1>&2
+	exit 1
+fi
