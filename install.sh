02e116c289b88eaf8c010643456af19cddfe9f9a
Joshua Young <156221811+WSU-Josh-Young@users.noreply.github.com>
2024-03-21 16:21:41 -0400

    inupdate

--- install.sh
+++ install.sh
@@ -6,6 +6,11 @@ if [[ "$(id -u)" -eq 0 ]]; then
 	echo "This script is run as root."
 	if [[ "$(which apt)" ]]; then
 		echo "apt is installed."
+		# updata apt package manager
+		apt update
+		# install packages with -y to automate through installations
+		apt install -y\
+			nmap\
 	else
 		echo "apt is not installed." 1>&2
 		exit 1
